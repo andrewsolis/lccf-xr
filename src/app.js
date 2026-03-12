e80212660885aa6531ec105fd2ef42beee161391
karenheckel <karen.heckel@utexas.edu>
2026-03-12 14:30:24 -0500

    Adding in image target data

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-const onxrloaded = () => {    XR8.XrController.configure({        imageTargetData: [            require('../image-targets/Target_Heat_Sink.json'),        ],    })}window.XR8 ? onxrloaded() : window.addEventListener('xrloaded', onxrloaded)
+const onxrloaded = () => {
+    XR8.XrController.configure({
+        imageTargetData: [
+            require('../image-targets/Target_Heat_Sink.json'),
+            require('../image-targets/Target_M2.json'),
+            require('../image-targets/Target_Riser_Card.json'),
+            require('../image-targets/Target_Motherboard.json'),
+            require('../image-targets/Target_Infiniband.json'),
+            require('../image-targets/Target_Superchip.json'),
+            require('../image-targets/Target_AC.json'),
+            require('../image-targets/Target_Delivery.json'),
+            require('../image-targets/Target_Elevators.json'),
+            require('../image-targets/Target_Govt_officials.json'),
+            require('../image-targets/Target_Library.json'),
+            require('../image-targets/Target_Roads.json'),
+        ],
+    })
+}
+window.XR8 ? onxrloaded() : window.addEventListener('xrloaded', onxrloaded)
