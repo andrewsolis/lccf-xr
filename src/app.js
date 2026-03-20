35f3cb76d3fcb318cb29c2646c6f7da202b2e894
karenheckel <karen.heckel@utexas.edu>
2026-03-19 18:33:37 -0700

    rename image target files

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 const onxrloaded = () => {
     XR8.XrController.configure({
         imageTargetData: [
-            require('../image-targets/Target_Heat_Sink.json'),
-            require('../image-targets/Target_M2.json'),
-            require('../image-targets/Target_Riser_Card.json'),
-            require('../image-targets/Target_Motherboard.json'),
-            require('../image-targets/Target_Infiniband.json'),
-            require('../image-targets/Target_Superchip.json'),
-            require('../image-targets/Target_AC.json'),
-            require('../image-targets/Target_Delivery.json'),
-            require('../image-targets/Target_Elevators.json'),
-            require('../image-targets/Target_Govt_officials.json'),
-            require('../image-targets/Target_Library.json'),
-            require('../image-targets/Target_Roads.json'),
+            require('../image-targets/heatsink.json'),
+            require('../image-targets/m2.json'),
+            require('../image-targets/riser_card.json'),
+            require('../image-targets/motherboard.json'),
+            require('../image-targets/infiniband.json'),
+            require('../image-targets/grace_hopper.json'),
+            require('../image-targets/heatsink_analogy.json'),
+            require('../image-targets/infiniband_analogy.json'),
+            require('../image-targets/riser_card_analogy.json'),
+            require('../image-targets/grace_hopper_analogy.json'),
+            require('../image-targets/m2_analogy.json'),
+            require('../image-targets/motherboard_analogy.json'),
         ],
     })
 }
